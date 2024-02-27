93faae5bf1c59837110e239bf27eff226b7904e2
daveekun <teaelvtars.work@gmail.com>
2024-02-27 22:24:43 +0200

    adding gizmos for distances and base class

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -12,8 +12,50 @@ public enum state
 
 public class EnemyAI : MonoBehaviour
 {
-    private Transform target = GameManager.instance.Player;
-    public state s = state.idle;
+    public Transform target;
+    public state State = state.idle;
+    public bool hasVision;
+    public float visionDistance;
+    public LayerMask wall;
+    public bool debug;
+    private bool drawRay = true;
+
+    private void OnDrawGizmos()
+    {
+        if (drawRay)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, (target.position - transform.position).normalized * visionDistance + transform.position);
+        }
+        if (debug)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, visionDistance);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(target.position, transform.position) < visionDistance)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, target.position - transform.position, visionDistance, wall);
+            if (hit)
+            { 
+                hasVision = false;
+                drawRay = false;
+            }
+            else
+            {
+                hasVision = true;
+                drawRay = true;
+            }
+        }
+        else
+        {
+            hasVision = false;
+            drawRay = false;
+        }
+    }
 
     void Attack()
     {
