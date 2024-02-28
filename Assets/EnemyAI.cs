dda166394e6746836d89b6ec72d45e5785408e27
daveekun <teaelvtars.work@gmail.com>
2024-02-28 17:14:08 +0200

    updated states to check last know pos

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -8,29 +8,40 @@ public enum state
     idle,
     chase,
     attack,
+    checkLast
 }
 
 public class EnemyAI : MonoBehaviour
 {
     public Transform target;
+    public Vector3 lastKnowPos;
     public state State = state.idle;
     public bool hasVision;
     public float visionDistance;
     public LayerMask wall;
     public bool debug;
-    private bool drawRay = true;
+
+    public float attackRange;
+    public float attackSpeed;
+
+    public float moveSpeed;
 
     private void OnDrawGizmos()
     {
-        if (drawRay)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(transform.position, (target.position - transform.position).normalized * visionDistance + transform.position);
-        }
         if (debug)
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(transform.position, visionDistance);
+            if (hasVision)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(transform.position, (target.position - transform.position).normalized * visionDistance + transform.position);
+            }
+            else
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, lastKnowPos);
+            }
         }
     }
 
@@ -42,33 +53,67 @@ public class EnemyAI : MonoBehaviour
             if (hit)
             { 
                 hasVision = false;
-                drawRay = false;
+                State = state.checkLast;
             }
             else
             {
+                lastKnowPos = target.position;
                 hasVision = true;
-                drawRay = true;
+                State = state.chase;
             }
         }
         else
         {
             hasVision = false;
-            drawRay = false;
+            State = state.checkLast;
+        }
+        if (hasVision)
+        {
+            if (Vector3.Distance(lastKnowPos, transform.position) < attackRange)
+                State = state.attack;
+            else
+                State = state.chase;
         }
     }
 
-    void Attack()
+    private void Update()
     {
-
+        switch (State)
+        {
+            case state.idle:
+                Idle();
+                break;
+            case state.attack:
+                Attack();
+                break;
+            case state.checkLast:
+                CheckLast();
+                break;
+            case state.chase:
+                Chase();
+                break;
+        }
     }
 
-    void Chase()
+    public void CheckLast()
     {
-
+        transform.position += (lastKnowPos - transform.position).normalized * Time.deltaTime * moveSpeed;
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
-    void Idle()
-    {
 
+    public virtual void Attack()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    public virtual void Chase()
+    {
+        transform.position += (target.position - transform.position).normalized * Time.deltaTime * moveSpeed;
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+    public virtual void Idle()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 }
 
